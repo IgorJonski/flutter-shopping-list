bc47bcbe56fe78ab23a62303a53dcd00462f407f
IgorJonski <jonskiigor@gmail.com>
2024-01-03 23:52:08 +0100

    Add NewItem screen, navigate to it on app bar action click

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
 class GroceryList extends StatelessWidget {
   const GroceryList({super.key});
+
+  void _addItem(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          return const NewItem();
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Grocery List'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              _addItem(context);
+            },
+          ),
+        ],
       ),
       body: ListView.builder(
         itemBuilder: (context, index) {
