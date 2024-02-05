b8880453a863e0c8f7ac1581871c7269cbd18d42
IgorJonski <jonskiigor@gmail.com>
2024-02-05 11:31:52 +0100

    Add item, display empty list text, delete items on swipe

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,22 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
 
-  void _addItem(BuildContext context) {
-    Navigator.of(context).push(
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem(BuildContext context) async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (context) {
           return const NewItem();
         },
       ),
     );
+
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
+  void _removeItem(int index) {
+    setState(() {
+      _groceryItems.removeAt(index);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget content = Center(
+      child: Text(
+        'No items added yet.',
+      ),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemBuilder: (context, index) {
+          return Dismissible(
+            key: ValueKey(_groceryItems[index].id),
+            onDismissed: (direction) {
+              _removeItem(index);
+            },
+            child: ListTile(
+              title: Text(_groceryItems[index].name),
+              leading: Container(
+                width: 24,
+                height: 24,
+                decoration: BoxDecoration(
+                  color: _groceryItems[index].category.color,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              trailing: Text('${_groceryItems[index].quantity}x'),
+            ),
+          );
+        },
+        itemCount: _groceryItems.length,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Grocery List'),
@@ -29,23 +83,7 @@ class GroceryList extends StatelessWidget {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(groceryItems[index].name),
-            leading: Container(
-              width: 24,
-              height: 24,
-              decoration: BoxDecoration(
-                color: groceryItems[index].category.color,
-                borderRadius: BorderRadius.circular(8),
-              ),
-            ),
-            trailing: Text('${groceryItems[index].quantity}x'),
-          );
-        },
-        itemCount: groceryItems.length,
-      ),
+      body: content,
     );
   }
 }
